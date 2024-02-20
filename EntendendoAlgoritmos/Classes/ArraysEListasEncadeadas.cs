11702f85eb764ca563863bb8f6a961d59684c098
Athos Schrapett <athosschrapett@outlook.com>
2024-02-20 07:51:54 -0300

    feat: Buscar menor em uma lista

---
+++ EntendendoAlgoritmos/Classes/ArraysEListasEncadeadas.cs
@@ -0,0 +1,20 @@
+﻿namespace EntendendoAlgoritmos.Classes;
+public class ArraysEListasEncadeadas
+{
+    public static int BuscaMenor(int[] arr)
+    {
+        int menor = arr[0];
+        int menorIndice = 0;
+
+        foreach (int i in Enumerable.Range(1, arr.Length - 1))
+        {
+            if (arr[i] < menor)
+            {
+                menor = arr[i];
+                menorIndice = i;
+            }
+        }
+
+        return menorIndice;
+    }
+}
--- EntendendoAlgoritmos/Program.cs
+++ EntendendoAlgoritmos/Program.cs
@@ -1,7 +1,9 @@
 ﻿using EntendendoAlgoritmos.Classes;
 
 int[] lista = [1, 3, 5, 7, 9];
+int[] listaDesordenada = [5, 8, 7, 3, 0, 5];
 
-Console.WriteLine(PesquisaBinaria.Pesquisa(lista, 3));
-Console.WriteLine(PesquisaBinaria.Pesquisa(lista, 1_888_700_100));
-Console.WriteLine(PesquisaBinaria.Pesquisa(lista, -1));
+//Console.WriteLine(PesquisaBinaria.Pesquisa(lista, 3));
+//Console.WriteLine(PesquisaBinaria.Pesquisa(lista, -1));
+
+Console.WriteLine(ArraysEListasEncadeadas.BuscaMenor(listaDesordenada));
